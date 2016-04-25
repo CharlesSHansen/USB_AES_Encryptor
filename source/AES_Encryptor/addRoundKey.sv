05710095a884ba19585e113266be964d3dd3baf9
Mentor Graphics <mg102@ee207lnx08.ecn.purdue.edu>
2016-04-24 23:39:41 -0400

    this is mitch's aes stuff so don't touch it bitches

---
+++ source/AES_Encryptor/addRoundKey.sv
@@ -0,0 +1,13 @@
+// $Id: $
+// File name:   addRoundKey.sv
+// Created:     4/24/2016
+// Author:      Mitchel Bouma
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: add round key block
+
+input addRoundKey();
+
+
+
+endmodule
