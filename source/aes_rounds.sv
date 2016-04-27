b16e0be453024a52bfff305b3ef728470a8d1220
Alex Dunker <adunker@purdue.edu>
2016-04-26 21:18:36 -0400

    All ten rounds of AES now working correctly

--- source/aes_rounds.sv
+++ source/aes_rounds.sv
@@ -15,9 +15,9 @@ module aes_rounds
    
    logic [0:127]  roundkey0, roundkey1, roundkey2, roundkey3, roundkey4, roundkey5, 
 		  roundkey6, roundkey7, roundkey8, roundkey9, roundkey10;
-   logic [0:127]  sub1, sub2, sub3, sub4, sub5, sub6, sub7, sub8, sub9;
-   logic [0:127]  shift1, shift2, shift3, shift4, shift5, shift6, shift7, shift8, shift9;
-   logic [0:127]  mix1, mix2, mix4, mix5, mix6, mix7, mix8, mix9;
+   logic [0:127]  sub1, sub2, sub3, sub4, sub5, sub6, sub7, sub8, sub9, sub10;
+   logic [0:127]  shift1, shift2, shift3, shift4, shift5, shift6, shift7, shift8, shift9, shift10;
+   logic [0:127]  mix1, mix2, mix3, mix4, mix5, mix6, mix7, mix8, mix9;
    logic [0:127]  add0, add1, add2, add3, add4, add5, add6, add7, add8, add9;
    
    assign roundkey0 = schedule[0:127];
@@ -39,7 +39,60 @@ module aes_rounds
    subBytes sub_1 (.inData(add0), .outData(sub1));
    shiftRows shift_1 (.inData(sub1), .outData(shift1));
    mixColumns mix_1 (.inData(shift1), .outData(mix1));
-   addRoundKey add_1 (.inData(mix1), .outData(add1));
+   addRoundKey add_1 (.inData(mix1), .roundKey(roundkey1), .outData(add1));
+
+   // Round 2
+   subBytes sub_2 (.inData(add1), .outData(sub2));
+   shiftRows shift_2 (.inData(sub2), .outData(shift2));
+   mixColumns mix_2 (.inData(shift2), .outData(mix2));
+   addRoundKey add_2 (.inData(mix2), .roundKey(roundkey2), .outData(add2));
+
+   // Round 3
+   subBytes sub_3 (.inData(add2), .outData(sub3));
+   shiftRows shift_3 (.inData(sub3), .outData(shift3));
+   mixColumns mix_3 (.inData(shift3), .outData(mix3));
+   addRoundKey add_3 (.inData(mix3), .roundKey(roundkey3), .outData(add3));
+
+   // Round 4
+   subBytes sub_4 (.inData(add3), .outData(sub4));
+   shiftRows shift_4 (.inData(sub4), .outData(shift4));
+   mixColumns mix_4 (.inData(shift4), .outData(mix4));
+   addRoundKey add_4 (.inData(mix4), .roundKey(roundkey4), .outData(add4));
+
+   // Round 5
+   subBytes sub_5 (.inData(add4), .outData(sub5));
+   shiftRows shift_5 (.inData(sub5), .outData(shift5));
+   mixColumns mix_5 (.inData(shift5), .outData(mix5));
+   addRoundKey add_5 (.inData(mix5), .roundKey(roundkey5), .outData(add5));
+
+   // Round 6
+   subBytes sub_6 (.inData(add5), .outData(sub6));
+   shiftRows shift_6 (.inData(sub6), .outData(shift6));
+   mixColumns mix_6 (.inData(shift6), .outData(mix6));
+   addRoundKey add_6 (.inData(mix6), .roundKey(roundkey6), .outData(add6));
+
+   // Round 7
+   subBytes sub_7 (.inData(add6), .outData(sub7));
+   shiftRows shift_7 (.inData(sub7), .outData(shift7));
+   mixColumns mix_7 (.inData(shift7), .outData(mix7));
+   addRoundKey add_7 (.inData(mix7), .roundKey(roundkey7), .outData(add7));
+
+   // Round 8
+   subBytes sub_8 (.inData(add7), .outData(sub8));
+   shiftRows shift_8 (.inData(sub8), .outData(shift8));
+   mixColumns mix_8 (.inData(shift8), .outData(mix8));
+   addRoundKey add_8 (.inData(mix8), .roundKey(roundkey8), .outData(add8));
+
+   // Round 9
+   subBytes sub_9 (.inData(add8), .outData(sub9));
+   shiftRows shift_9 (.inData(sub9), .outData(shift9));
+   mixColumns mix_9 (.inData(shift9), .outData(mix9));
+   addRoundKey add_9 (.inData(mix9), .roundKey(roundkey9), .outData(add9));
+
+   // Round 10
+   subBytes sub_10 (.inData(add9), .outData(sub10));
+   shiftRows shift_10 (.inData(sub10), .outData(shift10));
+   addRoundKey add_10 (.inData(shift10), .roundKey(roundkey10), .outData(round_out));
     
    
 endmodule // aes_rounds
--- source/tb_aes_key_expansion.sv
+++ source/tb_aes_key_expansion.sv
@@ -1,10 +1,10 @@
 // $Id: $
-// File name:   tb_aes_keySchedule.sv
-// Created:     4/25/2016
+// File name:   tb_aes_rounds.sv
+// Created:     4/26/2016
 // Author:      Alexander Dunker
 // Lab Section: 337-04
 // Version:     1.0  Initial Design Entry
-// Description: Test bench for the key schedule.
+// Description: Test bench for testing the rounds of AES.
 
 `timescale 1ns / 10ps
 
@@ -23,17 +23,17 @@ module tb_aes_key_expansion
    
    always
      begin
-	tb_clk = 1'b0;
-	#(CLK_PERIOD/2.0);
-	tb_clk = 1'b1;
-	#(CLK_PERIOD/2.0);
+  tb_clk = 1'b0;
+  #(CLK_PERIOD/2.0);
+  tb_clk = 1'b1;
+  #(CLK_PERIOD/2.0);
      end
 
    initial
      begin
-	@(posedge tb_clk);
-	tb_key = 128'h2b7e151628aed2a6abf7158809cf4f3c;
-	@(posedge tb_clk);
-	@(posedge tb_clk);		 
+  @(posedge tb_clk);
+  tb_key = 128'h2b7e151628aed2a6abf7158809cf4f3c;
+  @(posedge tb_clk);
+  @(posedge tb_clk);     
      end
-endmodule // tb_aes_keySchedule
+endmodule
